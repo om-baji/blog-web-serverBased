425d584d8c859428747cc7fb1737c22d6d7583e2
om-baji <ombaji124@gmail.com>
2024-09-13 14:40:45 +0530

    improved

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import HomePage from './pages/HomePage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HomePage />
   </StrictMode>,
 )
