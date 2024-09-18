acca99ca1efe5cdc2a5a1427f2853618d8c369da
om-baji <ombaji124@gmail.com>
2024-09-18 23:17:40 +0530

    schema

---
+++ backend/prisma/migrations/20240918174515_last_cut_pro/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Sr_no` on the `post` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "post" DROP COLUMN "Sr_no";
---
+++ backend/prisma/migrations/20240918174626_the_final_cut/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "post" ADD COLUMN     "Sr_no" SERIAL NOT NULL;
